f0752f6e3bd52a019515bac3b545436b9424d7dd
anjulituck@Anjulis-iMac.local <anjulituck@Anjulis-iMac.local>
2022-07-08 16:01:17 -0500

    added HTML routes to server.js instead; added middleware and made files static; looking for additional dev dependency to help with note taking

--- Develop/server.js
+++ Develop/server.js
@@ -1,17 +1,33 @@
-
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //represent the instance of express
 const app = express(); 
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+//for static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// HTML ROUTES
 
 //retrieving notes
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')))
 
 // retrieving index
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')))
+
+// API ROUTES
+
+
+
+
+
+
 
 
 // listening at this port and do this
