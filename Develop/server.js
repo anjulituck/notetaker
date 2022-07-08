c26c5b59555a9e22c0ccd0018c044d82e2b654b0
anjulituck@Anjulis-iMac.local <anjulituck@Anjulis-iMac.local>
2022-07-08 17:41:19 -0500

    trying to add to Heroku and resolve node issue

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const fs = require('fs');
+//universal unique identifier
+const { v4: uuidv4 } = require('uuid');
 const path = require('path');
 const port = process.env.PORT || 3000;
 
@@ -25,13 +27,19 @@ app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.htm
 
 // to save notes and add to db as json
 app.get('/api/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, '/db/db.json'))
+    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, '/db/db.json')));
+    res.json(notes);
 
 });
 
 // to add new notes and add to db as json
 app.post('/api/notes', (req,res) => {
-
+    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, '/db/db.json')));
+    const newNotes = req.body;    
+    newNotes.id = uuidv4();
+    notes.push(newNotes);
+    fs.writeFileSync('./db/db.json,', JSON.stringify(notes));
+    res.json(notes);
 
 });
 
@@ -40,7 +48,6 @@ app.delete('/api/notes/:id', (req,res) => {
 
 
 });
-
 
 
 // listening at this port and do this
